309128aeb5fed3fcd75c6b124f53f2eb863dd0e1
hs <2455230388@qq.com>
2025-04-22 22:01:04 +0800

    +FlowTest

---
+++ app/src/main/java/com/coroutine/FlowTest.kt
@@ -0,0 +1,34 @@
+package com.coroutine
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.take
+import kotlin.random.Random
+
+// 循环调用10次网络请求 获取的结果平方，然后过滤出偶数，最后取前2个
+class FlowTest {
+
+    suspend fun useFlow() {
+        val flow = flow {
+            repeat(10) {
+                emit(request())
+            }
+        }
+
+        flow.map { it * it }
+            .filter { it % 2 == 0 }
+            .take(2)
+            .onEach { println(it) }
+            .collect() // flow开始执行
+    }
+
+    suspend fun request(): Int {
+        delay(1000)
+        return Random.nextInt(100)
+    }
+
+}
